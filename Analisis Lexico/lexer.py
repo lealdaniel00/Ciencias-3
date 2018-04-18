30b6d0756579f36f2b96aa094c34820caa66bbca
DavidA0 <vinci456_@hotmail.com>
2018-04-17 19:22:50 -0500

    Add files via upload
    Asignacion

--- Analisis Lexico/lexer.py
+++ Analisis Lexico/lexer.py
@@ -1,27 +1,35 @@
 import ply.lex as lex
 
-tokens = ['PRESERVADA','DESIGUALDAD','VARIABLE','ASIGNACION','NUMBER','SUMA','RESTA','MULTIPLICACION', 'DIVISION','FINAL']
+tokens = ['PRESERVADA','DESIGUALDAD','VARIABLE','ASIGNACION','NUMERO','SUMA','RESTA','MULTIPLICACION', 'DIVISION','FINAL','ASIGNACION']
 pReserv =['SI','si','entonces','ENTONCES','repetir','REPETIR','para','PARA','hacer','HACER','SINO','sino','mientras','MIENTRAS','HASTA','hasta','fin','FIN']
-desigualdad=['<','>','<=','>=','==','!=','<--','menor','MENOR','mayor','MAYOR','diferente','DIFERENTE','menor','MENOR','mayor','MAYOR']
+asignacion=['<-','=']
+desigualdad=['<','>','<=','>=','==','!=','menor','MENOR','mayor','MAYOR','diferente','DIFERENTE','menor','MENOR','mayor','MAYOR']
 t_ignore = ' \t'
-t_ASIGNACION = r'='
 t_SUMA = r'\+'
 t_RESTA = r'-'
 t_MULTIPLICACION = r'\*'
 t_DIVISION = r'/'
 t_VARIABLE = r'[[a-zA-Z_][a-zA-Z0-9_]*'
-t_FINAL = r'fin' 
-def t_NUMBER(t):
+t_FINAL = r'fin'
+def t_NUMERO(t):
     r'\d+'
     t.value = int(t.value)
     return t
 def t_PRESERVADA(t):
     r'[[a-zA-Z_][a-zA-Z0-9_]*'
     return buscar(t)
+def t_ASIGNACION(t):
+    r'= | (<-)'
+    return buscar(t)
 def t_DESIGUALDAD(t):
     r'[[a-zA-Z_][a-zA-Z0-9_]*|(==) | (<) | (>) | (<=) | (>=) | (!=)'
     return buscar(t)
+
 def buscar(t):
+    for x in asignacion:
+        if x==t.value:
+            t.type='ASIGNACION'
+            return t
     for x in desigualdad:
         if x==t.value:
             t.type='DESIGUALDAD'
@@ -46,4 +54,3 @@ for expresion in listaExpresiones:
         tok = lex.token()
         if not tok: break
         print (str(tok.value) + " - " + str(tok.type))
-
