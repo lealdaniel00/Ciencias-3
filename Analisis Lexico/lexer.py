8efdac14c8bf02d64382be9afdbb45cfb1a97988
Thomas Avila <thomasdanielavila@gmail.com>
2018-04-17 20:50:47 -0500

    Update lexer.py

--- Analisis Lexico/lexer.py
+++ Analisis Lexico/lexer.py
@@ -1,15 +1,16 @@
 import ply.lex as lex
 
-tokens = ['PRESERVADA','RELACIONAL','VARIABLE','ASIGNACION','NUMERO','SUMA','RESTA','MULTIPLICACION', 'DIVISION','FINAL','ASIGNACION','POTENCIACION','MODULO']
 pReserv =['SI','si','entonces','ENTONCES','repetir','REPETIR','para','PARA','hacer','HACER','SINO','sino','mientras','MIENTRAS','HASTA','hasta','fin','FIN','ESCRIBIR','escribir','leer','LEER']
 relacional=['<','>','<=','>=','==','!=','menor','MENOR','mayor','MAYOR','diferente','DIFERENTE','menor','MENOR','mayor','MAYOR']
+tokens = ['PRESERVADA','RELACIONAL','VARIABLE','NUMERO','SUMA','RESTA','MULTIPLICACION', 'DIVISION','FINAL','ASIGNACION','POTENCIACION','MODULO']+pReserv
+
 t_ignore = ' \t'
+t_ASIGNACION = r'='
 t_SUMA = r'\+'
 t_RESTA = r'-'
 t_MULTIPLICACION = r'\*'
 t_DIVISION = r'/'
 t_POTENCIACION = r'\^'
-t_ASIGNACION = r'='
 t_MODULO = r'%'
 t_VARIABLE = r'[[a-zA-Z_][a-zA-Z0-9_]*'
 t_FINAL = r'fin'
@@ -40,7 +41,6 @@ def t_error(t):
     print("Caracter ilegal '%s'" % t.value[0])
     t.lexer.skip(1)
 lex.lex() # Build the lexer
-pClave=['si','entonces']
 archivo="archivo.txt"
 listaExpresiones = [x.strip('\n') for x in open(archivo, "r").readlines()]
 for expresion in listaExpresiones:
@@ -49,4 +49,3 @@ for expresion in listaExpresiones:
         tok = lex.token()
         if not tok: break
         print (str(tok.value) + " - " + str(tok.type))
-
